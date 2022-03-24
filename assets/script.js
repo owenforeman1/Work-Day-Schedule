40ae24e499e4edcaeb735de516a6623db887d5b9
Owen Foreman <owen4man@gmail.com>
2022-03-23 20:48:35 -0400

    finished if else statement

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd MMMM, Do"));
 
+
 /**
  * need function for save button
  * function= save contents of textarea to local sotrage
@@ -8,11 +9,33 @@ $("#currentDay").text(currentDay.format("dddd MMMM, Do"));
  * 
  */
 
-// loop style hour blocks
-var textStuff = $(".text-rows");
-console.log(textStuff);
-$.each(textStuff, function (idx, element) {
+// var saveButtonF = ("saveBtn");
+// saveButtonF.addEventListener("click", function (event) {
     
+// });
+
+
+// loop style hour blocks
+var currentHour = $(".hourBlock");
+$.each(currentHour, function (idx, element) {
+    var hourString = $(element).children().children().eq(1).attr("id");      
+    var momentHour =  moment(hourString,"ha");
+    var momentCurrentTime = moment().subtract("hours", 8);
+
+      if(momentHour.format("ha") == momentCurrentTime.format("ha")) {
+          $(element).children().children().eq(1).addClass("present")
+        console.log("present");
+        textArea = "present"
+      } else if (momentHour.isBefore(momentCurrentTime)) {
+            $(element).children().children().eq(1).addClass("past")
+        console.log("past");
+        textArea = "past"
+   
+    }else if (momentHour.isAfter(momentCurrentTime)) {
+          $(element).children().children().eq(1).addClass("future")
+        console.log("future");
+        textArea = "future";
+    }
     /**pseudo coding how to identify hour for each row
      *hard code each row with hour in id
      within loop check thew id of each hour 
@@ -20,5 +43,8 @@ $.each(textStuff, function (idx, element) {
      future = green
      jquery addClass
      */
-    console.log(element);
+    // console.log(element);
 });
+
+var currentHour = moment();
+$("#text9am").text(currentHour.format("h:a"));
